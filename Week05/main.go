11b9dfdd47e03172aaad8a049d19206065279516
SENSETIME\chenlong1 <chenlong1@sensetime.com>
2021-11-21 20:57:16 +0800

    week05

--- Week05/main.go
+++ Week05/main.go
@@ -1,26 +1,57 @@
 package main
 
 import (
-	_ "fmt"
+	"fmt"
 	"sync/atomic"
-	_ "time"
+	"time"
 )
+
+// 滑动计数器
+type SlidingCounter interface {
+	Sum() float64
+	Max() float64
+	Avg() float64
+	Increment(i float64)
+}
 
 type slidingCounter struct {
 	//以时间为key存储的窗口
-	windows map[int64]*uint32
+	windows map[int64]*window
 	//时间间隔
 	interval int64
 }
 
-func main() {
-	//模拟计数
-	counter := NewSlidingCounter(10)
-	for _, request := range []float64{1, 2, 3, 4, 5, 6, 7, 8, 9} {
-		counter.Increment(request)
+type window struct {
+	Value uint32
+}
 
+func NewSlidingCounter(interval int64) SlidingCounter {
+	return &slidingCounter{
+		windows:  make(map[int64]*window),
+		interval: interval,
+	}
+}
+
+func (sc *slidingCounter) getCurrentWindow() *window {
+	now := time.Now().Unix()
+
+	if w, found := sc.windows[now]; found {
+		return w
 	}
 
+	result := &window{}
+	sc.windows[now] = result
+	return result
+}
+
+func (sc *slidingCounter) removeOldWindows() {
+	notExpired := time.Now().Unix() - sc.interval
+
+	for timestamp := range sc.windows {
+		if timestamp <= notExpired {
+			delete(sc.windows, timestamp)
+		}
+	}
 }
 
 func (sc *slidingCounter) Increment(i float64) {
@@ -28,6 +59,53 @@ func (sc *slidingCounter) Increment(i float64) {
 		return
 	}
 
+	w := sc.getCurrentWindow()
 	atomic.AddUint32(&w.Value, 1)
+	sc.removeOldWindows()
+}
 
+func (sc *slidingCounter) Sum() float64 {
+	now := time.Now().Unix()
+
+	var sum uint32
+	for timestamp, window := range sc.windows {
+		if timestamp >= now-sc.interval {
+			fmt.Println(timestamp, "xxx", now-sc.interval, window.Value)
+			sum += window.Value
+		} else {
+			fmt.Println("xxx", window.Value)
+		}
+	}
+
+	return float64(sum)
+}
+
+func (sc *slidingCounter) Max() float64 {
+	now := time.Now().Unix()
+
+	var max float64
+	for timestamp, window := range sc.windows {
+		if timestamp >= now-sc.interval {
+			if float64(window.Value) > max {
+				max = float64(window.Value)
+			}
+		}
+	}
+	return max
+}
+
+func (sc *slidingCounter) Avg() float64 {
+	return sc.Sum() / float64(sc.interval)
+}
+
+func main() {
+	//模拟计数
+	// 窗口周期为10秒
+	counter := NewSlidingCounter(1)
+	for _, request := range []float64{1, 2, 3, 4, 5, 6, 7, 8, 9} {
+		counter.Increment(request)
+		time.Sleep(1 * time.Second)
+	}
+
+	fmt.Println(counter.Sum(), counter.Avg(), counter.Max())
 }
