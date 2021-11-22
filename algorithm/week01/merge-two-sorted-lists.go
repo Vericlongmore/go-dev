7c152bf658b49354cbf7574fcfd5f6fb5d59007a
SENSETIME\chenlong1 <chenlong1@sensetime.com>
2021-11-22 12:05:16 +0800

    week01

--- algorithm/week01/merge-two-sorted-lists.go
+++ algorithm/week01/merge-two-sorted-lists.go
@@ -1,1 +1,25 @@
 package main
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	head := &ListNode{}
+	cur := head
+	for l1 != nil || l2 != nil {
+		if l1 != nil && l2 != nil {
+			if l1.Val < l2.Val {
+				cur.Next = l1
+				l1 = l1.Next
+			} else {
+				cur.Next = l2
+				l2 = l2.Next
+			}
+			cur = cur.Next
+		} else if l1 != nil {
+			cur.Next = l1
+			break
+		} else {
+			cur.Next = l2
+			break
+		}
+	}
+	return head.Next
+}
