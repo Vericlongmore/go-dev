76b71790c070bd58ddedb6d53a2413b3359f212d
SENSETIME\chenlong1 <chenlong1@sensetime.com>
2021-11-22 12:01:46 +0800

    week01

--- algorithm/week01/plus-one.go
+++ algorithm/week01/plus-one.go
@@ -1,1 +1,12 @@
 package main
+
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i != -1; i-- {
+		digits[i]++
+		if digits[i]/10 == 0 {
+			return digits
+		}
+		digits[i] = digits[i] % 10
+	}
+	return append([]int{1}, digits...)
+}
